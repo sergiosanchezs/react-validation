5088815c29e358823c7962fcf7b79965bb4f897b
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-09-01 15:34:57 -0400

    integrating react-hook-form library to validate forms

--- src/components/SignInForm__Old.js
+++ src/components/SignInForm__Old.js
@@ -12,13 +12,14 @@ import {
   Typography,
 } from '@material-ui/core';
 import { CssTextField, useStyles } from '../styles';
-import { useForm, Controller } from 'react-hook-form';
 
 const SignInForm = () => {
   const classes = useStyles();
-  const { register, handleSubmit, errors, control } = useForm();
 
-  const onSubmit = data => console.log(data); //alert(JSON.stringify(data));
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(e.target);
+  };
 
   return (
     <Container component='main' maxWidth='xs'>
@@ -30,50 +31,29 @@ const SignInForm = () => {
         <Typography component='h1' variant='h4'>
           Sign in
         </Typography>
-        <form
-          className={classes.form}
-          noValidate
-          onSubmit={handleSubmit(onSubmit)}
-        >
+        <form className={classes.form} noValidate onSubmit={e => onSubmit(e)}>
           <CssTextField
             name='email'
             label='Email Address'
             variant='outlined'
             margin='normal'
-            inputRef={register({
-              required: 'You must provide the email address!',
-              pattern: {
-                value: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                message: 'You must provide a valid email address!',
-              },
-            })}
             autoComplete='email'
             className={classes.margin}
             fullWidth
+            required
             autoFocus
           />
-          {errors.email && (
-            <span className={classes.error}>{errors.email.message}</span>
-          )}
+
           <CssTextField
             name='password'
             label='Password'
             type='password'
             variant='outlined'
             margin='normal'
-            inputRef={register({
-              required: 'You must provide a password.',
-              minLenght: {
-                value: 6,
-                message: 'Your password must be greater than 6 characters',
-              },
-            })}
             fullWidth
+            required
             autoComplete='current-password'
           />
-          {errors.password && (
-            <span className={classes.error}>{errors.password.message}</span>
-          )}
 
           <Grid container>
             <Grid item xs>
@@ -85,11 +65,9 @@ const SignInForm = () => {
               <FormControlLabel
                 label='Remember me'
                 control={
-                  <Controller
+                  <Checkbox
                     className={classes.checkBox}
                     name='remember'
-                    as={Checkbox}
-                    control={control}
                     defaultValue={false}
                   />
                 }
